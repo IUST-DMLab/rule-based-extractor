d4507f153ac7bdda76a5a3bad76d22189e7b6fb5
Majid Asgari <majid.asgari@gmail.com>
2017-09-02 16:11:34 +0430

    update version

--- src/main/java/ir/ac/iust/dml/kg/raw/rulebased/TripleJsonProducer.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/rulebased/TripleJsonProducer.java
@@ -3,7 +3,7 @@ package ir.ac.iust.dml.kg.raw.rulebased;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import ir.ac.iust.dml.kg.raw.triple.RawTriple;
-import ir.ac.iust.dml.kg.raw.utils.PrefixService;
+import ir.ac.iust.dml.kg.raw.utils.URIs;
 import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
@@ -16,14 +16,14 @@ class TripleJsonProducer {
   static String generateTripleJson(List<RawTriple> tripleList) {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     List<TripleJson> tripleJsons = new ArrayList<TripleJson>();
-    final String resourcePrefix = PrefixService.INSTANCE.prefixToUri("fkgr");
+    final String resourcePrefix = URIs.INSTANCE.prefixedToUri(URIs.INSTANCE.getFkgResourcePrefix() + ":");
     for (RawTriple triple : tripleList) {
       TripleJson tripleJson = new TripleJson();
 
 
       tripleJson.setSubject(resourcePrefix + triple.getSubject().replace(" ", "_"));
       tripleJson.setObject(resourcePrefix + triple.getObject().replace(" ", "_"));
-      tripleJson.setPredicate(PrefixService.INSTANCE.prefixToUri(triple.getPredicate()));
+      tripleJson.setPredicate(URIs.INSTANCE.prefixedToUri(triple.getPredicate()));
       tripleJson.setSource(triple.getSourceUrl());
       tripleJsons.add(tripleJson);
     }
